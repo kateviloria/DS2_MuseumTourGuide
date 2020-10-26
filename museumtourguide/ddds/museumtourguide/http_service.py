0a86fe6820dc7c957cf542f5460b215c9904527d
Kate Viloria <kate@Kates-MacBook-Pro.local>
2020-10-26 20:02:40 +0100

    artist name and medium OK

---
+++ museumtourguide/ddds/museumtourguide/http_service.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+
+import json
+
+from flask import Flask, request
+from jinja2 import Environment
+from urllib.request import Request, urlopen
+
+app = Flask(__name__)
+environment = Environment()
+
+
+def jsonfilter(value):
+    return json.dumps(value)
+
+
+environment.filters["json"] = jsonfilter
+
+
+def error_response(message):
+    response_template = environment.from_string("""
+    {
+      "status": "error",
+      "message": {{message|json}},
+      "data": {
+        "version": "1.0"
+      }
+    }
+    """)
+    payload = response_template.render(message=message)
+    response = app.response_class(
+        response=payload,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+def query_response(value, grammar_entry):
+    response_template = environment.from_string("""
+    {
+      "status": "success",
+      "data": {
+        "version": "1.1",
+        "result": [
+          {
+            "value": {{value|json}},
+            "confidence": 1.0,
+            "grammar_entry": {{grammar_entry|json}}
+          }
+        ]
+      }
+    }
+    """)
+    payload = response_template.render(value=value, grammar_entry=grammar_entry)
+    response = app.response_class(
+        response=payload,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+def multiple_query_response(results):
+    response_template = environment.from_string("""
+    {
+      "status": "success",
+      "data": {
+        "version": "1.0",
+        "result": [
+        {% for result in results %}
+          {
+            "value": {{result.value|json}},
+            "confidence": 1.0,
+            "grammar_entry": {{result.grammar_entry|json}}
+          }{{"," if not loop.last}}
+        {% endfor %}
+        ]
+      }
+    }
+     """)
+    payload = response_template.render(results=results)
+    response = app.response_class(
+        response=payload,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+def validator_response(is_valid):
+    response_template = environment.from_string("""
+    {
+      "status": "success",
+      "data": {
+        "version": "1.0",
+        "is_valid": {{is_valid|json}}
+      }
+    }
+    """)
+    payload = response_template.render(is_valid=is_valid)
+    response = app.response_class(
+        response=payload,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+@app.route("/dummy_query_response", methods=['POST'])
+def dummy_query_response():
+    response_template = environment.from_string("""
+    {
+      "status": "success",
+      "data": {
+        "version": "1.1",
+        "result": [
+          {
+            "value": "dummy",
+            "confidence": 1.0,
+            "grammar_entry": null
+          }
+        ]
+      }
+    }
+     """)
+    payload = response_template.render()
+    response = app.response_class(
+        response=payload,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+@app.route("/action_success_response", methods=['POST'])
+def action_success_response():
+    response_template = environment.from_string("""
+   {
+     "status": "success",
+     "data": {
+       "version": "1.1"
+     }
+   }
+   """)
+    payload = response_template.render()
+    response = app.response_class(
+        response=payload,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+# Harvard Museums
+
+def get_data(painting_title, key="f315fe8e-b252-4a27-bdb3-0fb6dec18e67"):
+    url = f"https://api.harvardartmuseums.org/object/{painting_title}?apikey={key}"
+    request = Request(url)
+    response = urlopen(request)
+    data = response.read()
+    return json.loads(data)
+
+@app.route("/artist_name", methods=['POST'])
+def artist_name():
+    payload = request.get_json()
+    painting_title = payload["context"]["facts"]["painting_to_search"]["grammar_entry"]
+    data = get_data(painting_title)
+
+    # get artist name
+    if data["people"][0]["displayname"] == "Unknown Artist":
+      artist_name = "unknown"
+    else: # no data
+      artist_name = data["people"][0]["displayname"]
+
+    return query_response(value=artist_name, grammar_entry=None)
+
+@app.route("/medium", methods=['POST'])
+def medium():
+    payload = request.get_json()
+    painting_title = payload["context"]["facts"]["painting_to_search"]["grammar_entry"]
+    data = get_data(painting_title)
+
+    # get painting medium
+    medium = data["medium"]
+
+    return query_response(value=medium, grammar_entry=None)
